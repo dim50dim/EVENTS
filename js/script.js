e6394702459b04ca049a25fd1525af8acd1cf99d
dim50dim <zheliezkov@gmail.com>
2021-06-13 08:35:19 +0300

    add clear option

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
  
 let button = document.querySelector('.btn');
      let input = document.querySelector('.inp');
+
+
 button.addEventListener('click', function(){
     document.querySelector('.div1').innerHTML = input.value;
-    
+    document.querySelector('.div1').style.color = 'green';
+    input.value = '';
 });
