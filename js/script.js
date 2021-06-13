afddc7f8532a20b2d245bdd49f2728df808ffbce
dim50dim <zheliezkov@gmail.com>
2021-06-13 08:38:46 +0300

    change text colour

--- js/script.js
+++ js/script.js
@@ -5,6 +5,6 @@ let button = document.querySelector('.btn');
 
 button.addEventListener('click', function(){
     document.querySelector('.div1').innerHTML = input.value;
-    document.querySelector('.div1').style.color = 'green';
-    input.value = '';
+       document.querySelector('.div1').style.color = 'red';
+              input.value = '';
 });
