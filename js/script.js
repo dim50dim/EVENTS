ff94188799a908c9702b72ffd5a6fc5475e4379f
dim50dim <zheliezkov@gmail.com>
2021-06-15 08:20:17 +0300

    final fix

--- js/script.js
+++ js/script.js
@@ -1,13 +1,24 @@
     'use strict';
 
 
-let button = document.querySelector('.btn');
-     let input = document.querySelector('.inp');
-           let li = document.querySelector('.li');
 
+    
 
-button.addEventListener('click', function(){
-       li.innerHTML = input.value;
-       li.style.color = 'red';
-              input.value = '';
-});
+ let button = document.querySelector('.btn');
+ let input = document.querySelector('.inp');
+
+ button.addEventListener('click', function(){
+
+      
+        let li = document.createElement('li');
+          li.innerHTML = input.value;
+          
+          input.value = '';
+           li.style.color = `rgb(${putRandom(0, 255)}, ${putRandom(0, 255)}, ${putRandom(0, 255)})`;
+
+     function putRandom(min, max){
+         return Math.ceil(Math.random() * (max - min) + min);
+     }        
+        
+   document.querySelector('.ul').after(li);
+ });
